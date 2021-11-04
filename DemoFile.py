2bb0058cc9ddef3dcf243dcc2acdcd3f4269ee44
kaaya1 <kkk333444@naver.com>
2021-11-04 13:50:02 +0900

    정규표현식 1번

---
+++ DemoFile.py
@@ -0,0 +1,37 @@
+# DemoFile.py
+
+#파일에 쓰기(write + text)
+#mac, linux: /users/kim/Desktop?work
+#win: c:\work\test.txt 특수문자(탈출문자)
+f = open("c:\\work\\demo.txt", "wt")
+f.write("첫번째\n두번째\n세번째\n")
+#비피를 비우고 작업을 종료
+f.close()
+
+#파일을 읽기(read+text)
+f = open("c:\\work\\demo.txt","rt")
+print( f.readline())
+print( f.readline())
+print( "===하나의 문자열로 읽기---")
+print( f.tell())
+f.seek(0)
+result = f.read()
+print(result)
+print("===리스트로 받기===")
+f.seek(0)
+lst = f.readlines()
+print(lst)
+
+#파일 닫았나?
+if f.closed:
+    print("파일을 닫았습니다.")
+else:
+    f.close()
+
+#서식지정문자 (x: 헥사값(16진수), 이진수... 엑셀서식(, - 화폐단위 구분))
+print("{0:x}".format(10))
+print("{0:b}".format(10))
+print("{0:,}".format(15000))
+
+
+
---
+++ DemoRE.py
@@ -0,0 +1,8 @@
+#DemoRE.py
+
+import re
+
+result = re.search("[0-9]*th", "35th")
+print( result)
+print( result.group())
+
