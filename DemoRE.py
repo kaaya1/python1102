aeba848ccea9142fd830402da56ed8817f6b5537
kaaya1 <kkk333444@naver.com>
2021-11-04 15:15:20 +0900

    웹크롤링 연습

--- DemoRE.py
+++ DemoRE.py
@@ -2,7 +2,13 @@
 
 import re
 
+#찾았으면 매칭 객체 리턴
 result = re.search("[0-9]*th", "35th")
 print( result)
 print( result.group())
 
+#match 함수, search 함수 비교
+#match 함수는 정확하게 일치
+#search 함수는 포함하고 있을때
+print( bool(re.match("ap", "this is apple")))
+print( bool(re.search("ap", "this is apple")))
